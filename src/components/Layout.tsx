c8b0e02ed1d5493632579be0d61c499f8fe4cf14
implicitdef <implicitdef@users.noreply.github.com>
2022-10-01 19:15:11 +0200

    setup rough layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,18 +7,24 @@ type Props = {
 
 export function Layout({ children }: Props) {
   return (
-    <div>
+    <>
       <Head>
         <title>
           NosDéputés.fr : Observatoire citoyen de l'activité parlementaire
         </title>
-        {/* TODO revoir les métas, balises pour SEO, partage twitter, etc.
+        {/* TODO il faudra repasser sur les métas, balises pour SEO, partage twitter, etc.
          */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>Nos deputes header</header>
-      {children}
-      <footer>Footer</footer>
-    </div>
+      <div className="bg-slate-600">
+        <div className="container mx-auto flex min-h-screen flex-col bg-slate-300 px-4 py-4">
+          <header className="rounded bg-emerald-700 px-4 py-4">
+            Nos deputes header
+          </header>
+          <main className="grow px-8 py-8">{children}</main>
+          <footer className="rounded bg-emerald-700 px-4 py-4">Footer</footer>
+        </div>
+      </div>
+    </>
   )
 }
