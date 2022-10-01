1b26e80f2ea59e3c6df09f443a173218e78aba0a
implicitdef <implicitdef@users.noreply.github.com>
2022-10-01 19:15:11 +0200

    setup rough layout

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -25,5 +25,15 @@ export const getServerSideProps: GetServerSideProps<{ data: Data }> = async (
 export default function Page({
   data: { depute },
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return <div>Page du depute {depute.nom}</div>
+  return (
+    <div>
+      Page du depute {depute.nom}
+      <div className="mx-auto max-w-sm space-y-2 rounded-xl bg-white py-8 px-8 shadow-lg sm:flex sm:items-center sm:space-y-0 sm:space-x-6 sm:py-4">
+        <div className="space-y-0.5 text-center sm:text-left">
+          <p className="text-lg font-semibold text-black">{depute.nom}</p>
+          <p className="font-medium text-slate-500">{depute.groupe_acronyme}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+
+function BasicLink({ to }: { to: string }) {
+  return (
+    <Link href={to}>
+      <a className="block text-blue-500">{to}</a>
+    </Link>
+  )
+}
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <h1>Nos deputes homepage</h1>
+
+      <BasicLink to="/big" />
+      <BasicLink to="/deputes" />
+      <BasicLink to="/damien-abad" />
     </div>
   )
 }
