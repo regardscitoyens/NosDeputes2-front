695e80342f70520edfc63008a6d62250b2dca23c
implicitdef <implicitdef@users.noreply.github.com>
2022-10-01 19:10:11 +0200

    setup tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  return <h1>Nos deputes homepage</h1>
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>Nos deputes homepage</h1>
+    </div>
+  )
 }
 
 export default Home
