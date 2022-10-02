c5a51cf19bb95251670276037f680ea54fa86903
implicitdef <implicitdef@users.noreply.github.com>
2022-10-02 23:50:31 +0200

    general layout of deputes page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 import { Depute, fetchDeputes } from '../api/api'
+import { Todo } from '../components/Todo'
 
 type Data = {
   depute: Depute
@@ -26,12 +27,50 @@ export default function Page({
   data: { depute },
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <div>
-      Page du depute {depute.nom}
-      <div className="mx-auto max-w-sm space-y-2 rounded-xl bg-white py-8 px-8 shadow-lg sm:flex sm:items-center sm:space-y-0 sm:space-x-6 sm:py-4">
-        <div className="space-y-0.5 text-center sm:text-left">
-          <p className="text-lg font-semibold text-black">{depute.nom}</p>
-          <p className="font-medium text-slate-500">{depute.groupe_acronyme}</p>
+    <div className="grid grid-cols-12 gap-4">
+      <h1 className="col-span-full  text-center text-2xl">
+        <span className="font-bold">{depute.nom}</span>, député{' '}
+        <Todo inline>son groupe et sa circonscription</Todo>
+      </h1>
+      <div className="col-span-2">
+        <Todo>photo</Todo>
+      </div>
+      <div className="col-span-10">
+        <Todo>graph de présence et participation</Todo>
+      </div>
+      <div className="col-span-full">
+        <Todo>petite barre "Activité" avec diverses stats</Todo>
+      </div>
+
+      <div className="col-span-full grid grid-cols-2 gap-4">
+        <div className="space-y-8">
+          <Todo>
+            Informations générales, mandat, groupe, date de naissance,
+            professions, liens twitter etc.
+          </Todo>
+          <Todo>Adresses email, adresses postale, collaborateurs</Todo>
+          <Todo>
+            Responsabilités (commissions, missions, groupes extraparlementaires
+            etc.)
+          </Todo>
+          <Todo>
+            Travaux législatifs : ses derniers dossiers, interventions,
+            amendements (dont stats sur tous ses amendements)
+          </Todo>
+        </div>
+        <div className="space-y-8">
+          <Todo>
+            "Suivre l'activité du député" par email/rss/widget à embarquer
+          </Todo>
+          <Todo>Champ lexical (nuage de mots)</Todo>
+          <Todo>
+            Productions parlementaires (ses derniers rapports/props de lois)
+          </Todo>
+          <Todo>Votes (ses derniers votes)</Todo>
+          <Todo>
+            Questions au gouvernement (ses dernieres questions orales, écrites)
+          </Todo>
+          <Todo>Historique des fonctions et mandats</Todo>
         </div>
       </div>
     </div>
