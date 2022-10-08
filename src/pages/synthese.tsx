3d30b82abb219725570005cfbe32efc7b446d166
implicitdef <implicitdef@users.noreply.github.com>
2022-10-08 12:31:19 +0200

    suite

---
+++ src/pages/synthese.tsx
@@ -0,0 +1,25 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { Todo } from '../components/Todo'
+
+type Data = {}
+
+export const getServerSideProps: GetServerSideProps<{ data: Data }> = async (
+  context,
+) => {
+  return {
+    props: {
+      data: {},
+    },
+  }
+}
+
+export default function Page({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <Todo>
+      Synthèse générale de l'activité parlementaire depuis le début de la
+      législature
+    </Todo>
+  )
+}
