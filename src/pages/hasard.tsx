ecb184cb797118082628191b9ca3c1375a5e7c70
implicitdef <implicitdef@users.noreply.github.com>
2022-10-08 12:42:16 +0200

    suite

---
+++ src/pages/hasard.tsx
@@ -0,0 +1,25 @@
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { Todo } from '../components/Todo'
+
+type Data = {}
+
+export const getServerSideProps: GetServerSideProps<{ data: Data }> = async (
+  context,
+) => {
+  return {
+    props: {
+      data: {},
+    },
+  }
+}
+
+export default function Page({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return (
+    <Todo>
+      Un député au hasard: fetch les députes puis faire une redirection
+      javascript vers son URL ?
+    </Todo>
+  )
+}
