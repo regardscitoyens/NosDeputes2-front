5e5f6fb7ee3b4ec24cd272b4007364f975055c58
implicitdef <implicitdef@users.noreply.github.com>
2022-10-22 17:27:11 +0200

    fix thead/tbody

--- src/pages/organismes/groupe.tsx
+++ src/pages/organismes/groupe.tsx
@@ -32,28 +32,34 @@ export default function Page({
     <div className="space-y-4">
       <h1 className="text-center text-2xl">List des groupes politiques</h1>
       <table className="w-full bg-slate-100  text-slate-500">
-        <tr>
-          <th>Intitulé</th>
-          <th>Membres</th>
-        </tr>
-        {groupes.map(g => (
-          <tr key={g.id}>
-            <td className="py-2">
-              <Link href={`/groupe/${g.acronym}`}>
-                <a>
-                  <span
-                    className={`mx-2 inline-block py-1 px-2 text-white`}
-                    style={{ background: getColorForGroupeAcronym(g.acronym) }}
-                  >
-                    {g.acronym}
-                  </span>
-                  {g.nom}
-                </a>
-              </Link>
-            </td>
-            <td className="text-center">{g.deputesCount}</td>
+        <thead>
+          <tr>
+            <th>Intitulé</th>
+            <th>Membres</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {groupes.map(g => (
+            <tr key={g.id}>
+              <td className="py-2">
+                <Link href={`/groupe/${g.acronym}`}>
+                  <a>
+                    <span
+                      className={`mx-2 inline-block py-1 px-2 text-white`}
+                      style={{
+                        background: getColorForGroupeAcronym(g.acronym),
+                      }}
+                    >
+                      {g.acronym}
+                    </span>
+                    {g.nom}
+                  </a>
+                </Link>
+              </td>
+              <td className="text-center">{g.deputesCount}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
       <GrapheRepartitionGroupes groupesData={groupes} />
     </div>
