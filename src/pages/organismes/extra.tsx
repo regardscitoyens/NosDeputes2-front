a33696708a0059e73f1ac734c3a473c3590bd14d
implicitdef <implicitdef@users.noreply.github.com>
2022-10-24 21:16:53 +0200

    peaufinage groupes parlementaires

--- src/pages/organismes/extra.tsx
+++ src/pages/organismes/extra.tsx
@@ -1,9 +1,11 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import Link from 'next/link'
 import { Todo } from '../../components/Todo'
 import {
   OrganismeWithCounts,
   queryOrganismsList,
 } from '../../repositories/deputesAndOrganismesRepository'
+import { isCommissionPermanente } from '../../services/hardcodedData'
 
 type Data = { organismes: OrganismeWithCounts[] }
 
@@ -22,30 +24,33 @@ export default function Page({
   data,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   const { organismes } = data
+
   return (
-    <div>
-      <Todo inline>Trier cette table avec un ordre d'importance </Todo>
-      <Todo inline>Faire le lien vers la fiche pour chacun d'entre eux </Todo>
-      <table>
-        <thead>
-          <tr>
-            <th>Nom</th>
-            <th>Membres</th>
-            <th>Réunions publiques</th>
+    <table className="w-full">
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Membres</th>
+        </tr>
+      </thead>
+      <tbody>
+        {organismes.map(organisme => (
+          <tr key={organisme.id} className="odd:bg-slate-200">
+            <td>
+              <Link href={`/organisme/${organisme.slug}`}>
+                <a>{organisme.nom}</a>
+              </Link>
+            </td>
+            <td
+              className={
+                organisme.deputesCount == 0 ? 'italic text-slate-400' : ''
+              }
+            >
+              {organisme.deputesCount}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {organismes.map(o => (
-            <tr key={o.id}>
-              <td>{o.nom}</td>
-              <td>{o.deputesCount}</td>
-              <td>
-                <Todo inline>?</Todo>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   )
 }
