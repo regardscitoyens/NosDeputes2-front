9e241eb74d43af100cd4f34a50a32ff4c33960e4
implicitdef <implicitdef@users.noreply.github.com>
2022-12-17 23:09:42 +0100

    rough visualisation of sessions length

--- src/newPageModules/sessionList/SessionList.render.tsx
+++ src/newPageModules/sessionList/SessionList.render.tsx
@@ -18,11 +18,30 @@ export function Page({
         {sessions.map(s => {
           return (
             <li key={s.uid}>
-              {formatDate(s.start_date)} au {formatDate(s.end_date)}
+              {formatDate(s.start_date)} au {formatDate(s.end_date)} {s.kind}
+              <div
+                className="ml-2 inline-block bg-green-800 p-2"
+                style={{
+                  width: computeWidth(s),
+                }}
+              >
+                {' '}
+              </div>
             </li>
           )
         })}
       </ul>
     </div>
   )
+}
+
+function computeWidth(s: types.Session) {
+  const diff = new Date(s.end_date).getTime() - new Date(s.start_date).getTime()
+  const diffInDays = Math.round(diff / (1000 * 60 * 60 * 24))
+
+  const maxInDays = 365
+  const ratio = Math.max(Math.min(diffInDays, maxInDays), 0) / maxInDays
+  const percent = Math.max(ratio * 100, 1)
+
+  return `${percent}%`
 }
