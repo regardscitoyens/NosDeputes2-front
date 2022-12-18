7f4c66a1f42c92b443cad0010abb78a9d07a588c
implicitdef <implicitdef@users.noreply.github.com>
2022-12-18 15:23:54 +0100

    display ordre du jour de chaque séance

--- src/newPageModules/seanceList/SeanceList.render.tsx
+++ src/newPageModules/seanceList/SeanceList.render.tsx
@@ -1,13 +1,12 @@
+import groupBy from 'lodash/groupBy'
 import { LegislatureNavigation } from '../../components/LegislatureNavigation'
-import * as types from './SeanceList.types'
 import {
   formatDate,
   formatDateWithTimeAndWeekday,
   getWeek,
   getWeekYear,
 } from '../../lib/utils'
-import groupBy from 'lodash/groupBy'
-import { start } from 'repl'
+import * as types from './SeanceList.types'
 
 export function Page({
   sessionsWithSeances,
@@ -20,14 +19,14 @@ export function Page({
         currentLegislature={legislature}
         urlsByLegislature={legislatureNavigationUrls}
       />
-      <h1 className="text-2xl">Sessions</h1>
+      <h1 className="text-2xl">Séances, semaine par semaine</h1>
       <ul>
         {sessionsWithSeances.map(session => {
           const { uid, kind, start_date, end_date, seances } = session
 
           const seancesByWeek = groupBy(seances, _ => {
             const d = new Date(_.start_date)
-            return `${getWeekYear(d)} ${getWeek(d)}`
+            return `${getWeek(d)} de ${getWeekYear(d)}`
           })
 
           return (
@@ -39,7 +38,8 @@ export function Page({
               <ul className="pl-8">
                 {Object.entries(seancesByWeek).map(([week, seancesOfWeek]) => {
                   return (
-                    <li key={week} className="py-2">
+                    <li key={week} className="my-8 bg-slate-200 p-2 shadow-lg ">
+                      <h3 className="font-bold">Semaine {week} </h3>
                       <ul>
                         {seancesOfWeek.map(seance => {
                           const { uid, start_date } = seance
@@ -47,10 +47,19 @@ export function Page({
                           return (
                             <li
                               key={uid}
-                              className={isFuture ? 'text-slate-500' : ''}
+                              className={isFuture ? 'text-yellow-700' : ''}
                             >
                               Séance du{' '}
                               {formatDateWithTimeAndWeekday(start_date)}
+                              <ul className="ml-8 italic text-slate-500">
+                                {seance.ordre_du_jour.map(point => {
+                                  return (
+                                    <li key={point.uid}>
+                                      <PointOrdreDuJour {...{ point }} />
+                                    </li>
+                                  )
+                                })}
+                              </ul>
                             </li>
                           )
                         })}
@@ -65,4 +74,65 @@ export function Page({
       </ul>
     </div>
   )
+}
+
+function PointOrdreDuJour({ point }: { point: types.PointOdjFinal }) {
+  const kind =
+    point.typePointOdj === 'Suite de la discussion'
+      ? 'Discussion (suite)'
+      : point.typePointOdj
+
+  function simplifyObjet(s: string) {
+    switch (kind) {
+      case 'Discussion':
+        return withoutPrefixes(s, 'Discussion du', 'Discussion de la')
+      case 'Discussion (suite)':
+        return withoutPrefixes(
+          s,
+          'Suite de la discussion du',
+          'Suite de la discussion de la',
+        )
+      case 'Questions au Gouvernement':
+        return s === 'Questions au Gouvernement' ? '' : s
+    }
+    return s
+  }
+
+  const objet = capitalizeFirstLetter(simplifyObjet(point.objet).trim())
+
+  const { dossierLegislatifRef } = point
+  return (
+    <span>
+      <span className="mr-1 bg-green-100 px-1">{kind}</span>
+      {objet} <DossierLegislatifRef {...{ dossierLegislatifRef }} />
+    </span>
+  )
+}
+
+function DossierLegislatifRef({
+  dossierLegislatifRef,
+}: {
+  dossierLegislatifRef?: string
+}) {
+  if (dossierLegislatifRef) {
+    return <span className="font-bold">{dossierLegislatifRef}</span>
+  }
+  return null
+}
+
+function capitalizeFirstLetter(s: string) {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+function withoutPrefixes(s: string, ...prefixes: string[]): string {
+  let res = s
+  prefixes.forEach(p => {
+    res = withoutPrefix(res, p)
+  })
+  return res
+}
+
+function withoutPrefix(s: string, prefix: string): string {
+  if (s.startsWith(prefix)) return s.slice(prefix.length)
+  return s
 }
