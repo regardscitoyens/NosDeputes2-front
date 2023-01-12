4822ba2f0e4d9e5d99e20580e5c694c555f74099
implicitdef <implicitdef@users.noreply.github.com>
2023-01-12 23:04:59 +0100

    mobile menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import { LATEST_LEGISLATURE } from '../lib/hardcodedData'
 import { MyLink } from './MyLink'
 
@@ -36,32 +36,57 @@ function Logo() {
   )
 }
 
-function SideMenu() {
+function Division({ children }: { children: ReactNode }) {
   return (
-    <nav className="flex w-72 flex-col border-r border-slate-400 bg-slate-300 text-center">
-      <div className="mx-4 border-b border-slate-400">
+    <div className="mx-4 border-b border-slate-400 last:border-b-0">
+      {children}
+    </div>
+  )
+}
+
+function SideMenu({ mobileMenuFolded }: { mobileMenuFolded: boolean }) {
+  return (
+    <nav
+      className={`fixed flex  w-screen flex-col border-r border-slate-400 bg-slate-300 text-center lowercase  md:static md:w-72 md:uppercase ${
+        mobileMenuFolded ? 'hidden md:block' : ''
+      }`}
+    >
+      <Division>
         <Logo />
-      </div>
-      <div className="mx-4 border-b border-slate-400">
+      </Division>
+      <Division>
         <MenuLink to="/deputes" label="Les députés" />
         <MenuLink to="/circonscription" label="Les circonscriptions" />
         <MenuLink to="/sessions" label="Les sessions parlementaires" />
         <MenuLink to="/seances" label="Les séances en hémicycle" />
         <MenuLink to="/dossiers" label="Les dossiers législatifs" />
         <MenuLink to="/scrutins" label="Les scrutins" />
-      </div>
-      <div className="mx-4">
+      </Division>
+      <Division>
         <MenuLink to="/faq" label="Questions fréquentes (FAQ)" />
         <MenuLink to="/mentions-legales" label="Mention légales" />
-      </div>
+      </Division>
     </nav>
+  )
+}
+
+function MobileTopBar({ toggleMobileMenu }: { toggleMobileMenu: () => void }) {
+  return (
+    <div className="bg-white md:hidden">
+      <button
+        className="m-1 rounded bg-slate-300 p-1"
+        onClick={toggleMobileMenu}
+      >
+        hamburger
+      </button>
+    </div>
   )
 }
 
 function RestOfPage({ children }: Props) {
   return (
     <div className="grow bg-slate-300 ">
-      <main className="container mx-auto flex min-h-screen flex-col pt-4 pb-6">
+      <main className="container mx-auto flex  flex-col pt-4 pb-6">
         {children}
       </main>
     </div>
@@ -69,6 +94,8 @@ function RestOfPage({ children }: Props) {
 }
 
 export function Layout({ children }: Props) {
+  const [mobileMenuFolded, setMobileMenuFolded] = useState(true)
+
   return (
     <>
       <Head>
@@ -80,9 +107,16 @@ export function Layout({ children }: Props) {
         {/* favicon commenté pour le moment car il m'embrouille dans mes onglets */}
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <div className="flex text-slate-800">
-        <SideMenu />
-        <RestOfPage {...{ children }} />
+      <div className="flex min-h-screen flex-col text-slate-800">
+        <MobileTopBar
+          toggleMobileMenu={() => {
+            setMobileMenuFolded(v => !v)
+          }}
+        />
+        <div className="flex grow">
+          <SideMenu {...{ mobileMenuFolded }} />
+          <RestOfPage {...{ children }} />
+        </div>
       </div>
     </>
   )
