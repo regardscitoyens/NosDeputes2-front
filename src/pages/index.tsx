d2e41674af70e2130eb8771fb77575aa8e5c9cc2
implicitdef <implicitdef@users.noreply.github.com>
2023-01-12 23:04:59 +0100

    clean up hp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <p>homepage</p>
+  return (
+    <div className="text-center">
+      <p className="mt-44">Page d'accueil. TBD</p>
+    </div>
+  )
 }
 
 export default Home
